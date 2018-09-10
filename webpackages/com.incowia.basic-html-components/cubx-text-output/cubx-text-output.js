fbc1d268e1f78c4ea303da9d03fd39e047b07d9e
edwingamboa <edwingamboa355@gmail.com>
2018-09-10 12:42:25 -0500

    #5 Migrate cubx-text-output

--- webpackages/com.incowia.basic-html-components/cubx-text-output/cubx-text-output.js
+++ webpackages/com.incowia.basic-html-components/cubx-text-output/cubx-text-output.js
@@ -1,35 +1,73 @@
 (function () {
   'use strict';
 
-  CubxPolymer({
+  CubxComponent({
     is: 'cubx-text-output',
 
-    created: function () {
+    contextReady: function () {
+      this.updateValue(this.getValue());
+      this.updateVisibility(this.getVisible());
+      this.updateLang(this.getLang());
+      this.updateId(this.getId());
     },
 
-    ready: function () {
-      this.value = this.getValue();
-    },
-
-    attached: function () {
-    },
-
-    cubxReady: function () {
+    modelIdChanged: function (id) {
+      this.updateId(id);
     },
 
     modelValueChanged: function (newValue) {
-      this.value = newValue;
+      this.updateValue(newValue);
+    },
+
+    modelVisibleChanged: function (newValue) {
+      this.updateVisibility(newValue);
     },
 
     __isHidden: function() {
-      return !this.model.visible;
+      return !this.getVisible();
     },
 
     /**
      *  Called when slot 'lang' has changed
      */
     modelLangChanged: function (lang) {
-      this.setAttribute('lang', lang);
+      this.updateLang(lang);
+    },
+
+    updateValue: function (value) {
+      if (value){
+        this.getMainHTMLElement().innerHTML = value;
+      }
+    },
+
+    updateVisibility: function(isVisible) {
+      if (!isVisible) {
+        this.setAttToMainHTMLElement('hidden', 'hidden');
+      } else {
+        this.removeAttToMainHTMLElement('hidden')
+      }
+    },
+
+    updateLang: function(lang) {
+      this.setAttToMainHTMLElement('lang', lang);
+    },
+
+    updateId: function(id) {
+      this.setAttToMainHTMLElement('id', id);
+    },
+
+    setAttToMainHTMLElement: function(att, val) {
+      if (val !== undefined) {
+        this.getMainHTMLElement().setAttribute(att, val);
+      }
+    },
+
+    removeAttToMainHTMLElement: function(att) {
+      this.getMainHTMLElement().removeAttribute(att);
+    },
+
+    getMainHTMLElement: function() {
+      return this.$$('span');
     }
   });
 }());
