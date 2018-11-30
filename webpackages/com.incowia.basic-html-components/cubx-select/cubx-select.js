110c7af8747ca82d229dc0d8cc2f78d8b26d761d
Judit Ross <jtrs@incowia.com>
2018-11-30 13:34:27 +0100

    #7 added slot "multiple"

--- webpackages/com.incowia.basic-html-components/cubx-select/cubx-select.js
+++ webpackages/com.incowia.basic-html-components/cubx-select/cubx-select.js
@@ -43,8 +43,12 @@
      * @param {event} event
      */
     inputFieldSlotValueChanged: function (event) {
-      // update the cubbles-model
-      this.setValue(event.target.value);
+      if (this.$$('select').getAttribute('multiple')) {
+        this.setValue(this._getSelectValues(this.$$('select')));
+      } else {
+        // update the cubbles-model
+        this.setValue(event.target.value);
+      }
     },
 
     /**
@@ -103,6 +107,17 @@
     },
 
     /**
+     *  Called when slot 'required' has changed
+     */
+    modelMultipleChanged: function (multiple) {
+      if (multiple) {
+        this.$$('select').setAttribute('multiple', true);
+      } else {
+        this.$$('select').removeAttribute('multiple');
+      }
+    },
+
+    /**
      * Fill the options of the select component
      * @private
      */
@@ -112,12 +127,12 @@
       var value;
       var text;
       for (var i in options) {
-        value = options[i][0];
-        text = options[i][1];
+        value = options[ i ][ 0 ];
+        text = options[ i ][ 1 ];
         if (value === this.getValue()) {
-          this.$$('select').options[i] = new Option(text, value, false, true);
+          this.$$('select').options[ i ] = new Option(text, value, false, true);
         } else {
-          this.$$('select').options[i] = new Option(text, value, false, false);
+          this.$$('select').options[ i ] = new Option(text, value, false, false);
         }
       }
     },
@@ -128,6 +143,21 @@
      */
     _emptySelect: function () {
       this.$$('select').options.length = 0;
+    },
+
+    _getSelectValues: function (select) {
+      var result = [];
+      var options = select && select.options;
+      var opt;
+
+      for (var i = 0, iLen = options.length; i < iLen; i++) {
+        opt = options[ i ];
+
+        if (opt.selected) {
+          result.push(opt.value || opt.text);
+        }
+      }
+      return result;
     }
   });
 }());
